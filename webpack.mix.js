e6155c8a253c851264f2f1a41c740d8ea102a213
mostafaznv <mostafa.zeinivand@gmail.com>
2023-05-17 13:34:37 +0330

    fix webpack.mix.js and nova.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,21 +1,13 @@
 const mix = require('laravel-mix')
-const path = require('path')
+
+require('./nova.mix')
 
 mix.setPublicPath('dist')
     .js('resources/js/field.js', 'js')
     .sass('resources/sass/field.scss', 'css')
     .vue({version: 3})
-    .webpackConfig({
-        externals: {
-            vue: 'Vue',
-        },
-        resolve: {
-            symlinks: false
-        }
-    })
-    .alias({
-        'laravel-nova': path.join(
-            __dirname,
-            '../../../vendor/laravel/nova/resources/js/mixins/packages.js'
-        )
-    })
+    .nova('mostafaznv/nova-video')
+
+if (mix.inProduction()) {
+    mix.version()
+}
